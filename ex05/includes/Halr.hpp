92c120348dc4ead8875762ad447a329377edccdc
root <root@LAPTOP-QTQ73EU2.localdomain>
2022-11-05 20:32:13 +0100

    ex05 - Harl done

--- ex05/includes/Halr.hpp
+++ ex05/includes/Halr.hpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:50:37 by root              #+#    #+#             */
-/*   Updated: 2022/11/02 17:51:46 by root             ###   ########.fr       */
+/*   Updated: 2022/11/05 20:24:09 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,23 @@
 # define HARL_H
 
 #include <iostream>
+#include <cstdlib>
 
 class	Harl;
 
 class Harl {
 	public:
 
+	Harl(void);
+	~Harl(void);
+	
+	void	complain(std::string level);
+	
 	private:
+	void	debug(void);
+	void	warning(void);
+	void	info(void);
+	void	error(void);
 };
 
 #endif
