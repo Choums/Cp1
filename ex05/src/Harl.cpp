fe0bc8ef2b502df1a18376bb89fb0524689944d0
root <root@LAPTOP-QTQ73EU2.localdomain>
2022-11-06 20:57:01 +0100

    ex05 - faire la conversion string int pour switch

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 18:30:54 by root              #+#    #+#             */
-/*   Updated: 2022/11/05 20:26:42 by root             ###   ########.fr       */
+/*   Updated: 2022/11/06 19:58:28 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,17 +37,17 @@ void	Harl::complain(std::string level)
 
 void	Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do !" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error(void)
--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -6,33 +6,21 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:50:39 by root              #+#    #+#             */
-/*   Updated: 2022/11/05 20:30:54 by root             ###   ########.fr       */
+/*   Updated: 2022/11/06 18:35:03 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Halr.hpp"
 
-int	main(void)
+int	main(int ac, char **av)
 {
+	if (ac != 2)
+	{
+		std::cout << "[ Invalid number of arguments ]" << std::endl;
+		return (1);
+	}
 	Harl	harl;
+	std::string str = av[1];
+	harl.complain(str);
 
-	std::cout << "DEBUG: ";
-	harl.complain("0");
-	std::cout << std::endl;
-	
-	std::cout << "WARNING: ";
-	harl.complain("1");
-	std::cout << std::endl;
-	
-	std::cout << "INFO: ";
-	harl.complain("2");
-	std::cout << std::endl;
-
-	std::cout << "ERROR: ";
-	harl.complain("3");
-	std::cout << std::endl;
-
-	std::cout << "ERROR: ";
-	harl.complain("3");
-	std::cout << std::endl;
 }
